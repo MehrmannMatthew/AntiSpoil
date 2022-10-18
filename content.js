394a3303d555aca31d79ecf7ffaec0d4b7449bf7
kuebler324 <71736302+kuebler324@users.noreply.github.com>
2022-10-18 10:18:32 -0500

    manifest and content

---
+++ content.js
@@ -0,0 +1,24 @@
+function replace() {
+
+    const tags = ["p", "span", "h", "h3", "a"];
+
+    const spoiler = "<h1 style=\"color:#FF0000\">[SPOILER]</h1>";
+
+    chrome.storage.local.get(['banned'], ({ banned }) => {
+
+        const modifiedBanned = banned.map(phrase => phrase.toLowerCase().trim());
+        
+        tags.forEach(tag => {
+            const currentTag = document.getElementsByTagName(tag);
+            for(const elt of currentTag) {
+                modifiedBanned.forEach(phrase => {
+                    if(elt.innerHTML.toLowerCase().indexOf(phrase) !== -1) {
+                        elt.innerHTML = spoiler;
+                    }
+                });
+            }
+        });
+    });
+}
+window.onload = replace;
+window.onscroll = replace;
