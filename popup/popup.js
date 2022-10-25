fc1b6f0e00e5dcd8d5cb4d600ab41264cb0d870d
kuebler324 <71736302+kuebler324@users.noreply.github.com>
2022-10-25 10:51:00 -0500

    Add files via upload
    popup html/js and init css

--- popup/popup.js
+++ popup/popup.js
@@ -2,6 +2,22 @@ function $(a, b) {
     return typeof b === 'number' ? document.querySelectorAll(a)[b] : document.querySelectorAll(a);
 }
 
+function createElement(parent, elementType, attributes, innerText) {
+    console.log(elementType, attributes);
+    const element = document.createElement(elementType);
+    if(parent) {
+        parent.appendChild(element);
+    }
+    for(const key in attributes) {
+        console.log(key);
+        element.setAttribute(key, attributes[key]);
+    }
+    if(innerText) {
+        element.innerText = innerText;
+    }
+    return element;
+}
+
 class Store {
     constructor() {
         this.banned = null;
@@ -11,39 +27,61 @@ class Store {
         });
 
     }
-    addPhrase(phrase) {
-        this.banned.push(phrase);
+    addPhrase(keyPhrase) {
+        this.banned.push(new Phrase(keyPhrase));
         this.update(true);
     }
     removePhrase(index) {
         this.banned.splice(index, 1);
         this.update(true);
     }
+    removeRelatedPhrase(index, relatedIndex) {
+
+    }
     update(changeStorage) {
         if(changeStorage) {
             chrome.storage.local.set({ 'banned': this.banned });
         }
-        const container = $('#blocked-phrases', 0);
-        container.innerHTML = '';
+        const bannedListContainer = $('#banned-list-container', 0);
+        bannedListContainer.innerHTML = '';
         for(let i = 0; i < this.banned.length; ++i) {
-            const phrase = this.banned[i];
-            const item = document.createElement('div');
-            const button = document.createElement('input');
-            button.setAttribute('type', 'button');
-            button.setAttribute('value', 'remove');
-            button.addEventListener('click', () => {
+            const { keyPhrase, relatedPhrases } = this.banned[i];
+            const itemContainer = createElement(bannedListContainer, 'div', { id: `banned-item-container-${i}`, class: 'banned-item-container' });
+
+            const keyPhraseContainer = createElement(itemContainer, 'div', { class: 'banned-phrase-container' });
+            createElement(keyPhraseContainer, 'div', { class: 'banned-remove-button' }, 'delete').addEventListener('click', () => {
                 this.removePhrase(i);
-            })
-            const span = document.createElement('span');
-            span.innerText = phrase;
-            item.appendChild(button);
-            item.appendChild(span);
-            container.appendChild(item);
+            });
+            createElement(keyPhraseContainer, 'div', { class: 'banned-phrase' }, keyPhrase);
+            createElement(keyPhraseContainer, 'div', { class: 'banned-expansion-button' }, 'expand').addEventListener('click', () => {
+                // expand
+            });
+
+            const relatedListContainer = createElement(itemContainer, 'div', { id: `banned-related-container-${i}`, class: 'banned-item-container' });
+            console.log(relatedPhrases);
+            relatedPhrases.forEach((phrase, relatedIndex) => {
+                const relatedItemContainer = createElement(relatedListContainer, 'div', { class: 'banned-phrase-container' });
+                createElement(relatedItemContainer, 'div', { class: 'banned-remove-button' }, 'delete').addEventListener('click', () => {
+                    this.removeRelatedPhrase(i, relatedIndex);
+                });
+                createElement(relatedItemContainer, 'div', { class: 'banned-phrase' }, phrase);
+            });
         }
     }
 }
 
 const store = new Store();
+
+class Phrase {
+    constructor(keyPhrase) {
+        this.keyPhrase = keyPhrase;
+        this.relatedPhrases = [
+            'related 1',
+            'related 2',
+            'related 3'
+        ];
+    }
+}
 
 window.onload = () => {
     const inputButton = () => {
