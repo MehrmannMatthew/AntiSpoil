f1ec62d683921b8c75b0d09b3b0f85484681fb8f
kuebler324 <71736302+kuebler324@users.noreply.github.com>
2022-11-10 11:52:55 -0600

    Add files via upload

--- components/message-system.js
+++ components/message-system.js
@@ -1,1 +1,22 @@
+class MessageSystem {
+  constructor() {
+    this.handlers = {};
+    chrome.runtime.onMessage.addListener(({ signature, body }, sender) => {
+      const handler = this.handlers[signature];
+      if(handler) {
+        handler(body);
+      }
+    });
+  }
+  addHandler(signature, handler) {
+    this.handlers[signature] = handler;
+  }
+  send(signature, body) {
+    chrome.runtime.sendMessage({
+      signature,
+      body
+    });
+  }
+}
 
+export default MessageSystem;
