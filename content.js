2777fc4602bc42612166faa4fc0fd70500d41b30
Kieran Delaney <kdelaney472@gmail.com>
2022-11-29 14:49:01 -0600

    fixed firefox support

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 const className = 'anti-spoil-blur-1aa2e7f0be3cb32e13e5c84d42bb23c7';
 var oldPreviousReplaceClass = generatePRC();
-const extensionContext = window.chrome || window.browser;
+const extensionContext = (chrome ? chrome : browser);
 
 extensionContext.runtime.onMessage.addListener(({ signature }) => {
     if(signature === 'replace') {
